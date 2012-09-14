8a366c6f179864c0126b17040217d07d95df8722
Aurélien AMILIN <aurelien.amilin@gmail.com>
2012-09-14 15:47:31 +0200

    Initial Commit

---
+++ WSTKit/WSTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WSTAppDelegate.h
+//  WSTKit
+//
+//  Created by Aurélien AMILIN on 14/09/12.
+//  Copyright (c) 2012 Aurélien AMILIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WSTKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WSTKit
+//
+//  Created by Aurélien AMILIN on 14/09/12.
+//  Copyright (c) 2012 Aurélien AMILIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WSTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WSTAppDelegate class]));
+    }
+}
